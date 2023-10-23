1641ad58ffd585dd9e864b31e8a83832db19e28a
Nicolas Sanatana Kruger <nicolassantanakruger@gmail.com>
2023-10-22 23:07:48 -0300

    add: theme to global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import { MaxWidthWrapper } from "@/components/MaxWidthWrapper"
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="bg-red-500" ><div>Hi</div></MaxWidthWrapper>
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-300 bg-white px-7 py-2 shadow-md backdrop-blur trasition-all hover:border-gray-500 hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700">
+          Quill is now public!!
+        </p>
+      </div>
+    </MaxWidthWrapper>
   )
 }
