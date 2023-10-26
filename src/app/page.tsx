7b1b4811d8cfb608cb8f93ce93bb2ced43e31c13
Nicolas Sanatana Kruger <nicolassantanakruger@gmail.com>
2023-10-26 17:30:24 -0300

    add: dashboard preview to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper"
  
 import  Link from "next/link"
+import Image from 'next/image'
 
 import { ArrowRight } from "lucide-react"
 import { buttonVariants } from "@/components/ui/button"
@@ -43,7 +44,22 @@ export default function Home() {
         </div>
       </div>
 
-      
+      <div>
+        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <Image 
+                src="/dashboard-preview.jpg" 
+                alt="product preview" 
+                width={1364} 
+                height={866} 
+                quality={100}
+                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
